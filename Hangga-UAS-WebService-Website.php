2e5603236c119f57332baa9f157c69944db4fc65
PeTIK <hanggakristanto99@gmail.com>
2019-05-24 02:13:25 +0700

    kedua

--- Hangga-UAS-WebService-Website.php
+++ Hangga-UAS-WebService-Website.php
@@ -4,7 +4,8 @@
  $data = json_decode($konten, true);
 
  //echo $data[1]["nama_lokasi"];
- echo "<h1 align='center'>Cuaca hari ini yang terdapat pada ".count($data)." wilayah</h1>";
+ echo "<br/>";
+ echo "<h1 align='center'>Cuaca Hari Ini <br> Di ".count($data)." wilayah Indonesia</h1>";
  echo "<br/>";
 ?>
 
@@ -15,39 +16,40 @@
  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+
  <div class="container">
   <div class="row">
     <div class="col-md-12">
        <table class="table table-striped">
-  <thead class="thead-dark">
-    <tr>
-   <th>No</th>
-   <th>Kota</th>
-   <th>Siang</th>
-   <th>Malam</th>
-   <th>Dini hari</th>
-   <th>Suhu</th>
-   <th>Kelembapan</th> 
-    </tr>
-  </thead>
-  <tbody>
-  <?php   
-   for($a=1; $a < count($data); $a++)
-   {
-    print "<tr>";
-    // penomeran otomatis
-    print "<td>".$a."</td>";
-    // menayangkan 
-    print "<td>".$data[$a]['Kota']."</td>";
-    print "<td>".$data[$a]['siang']."</td>";
-    print "<td>".$data[$a]['malam']."</td>";
-    print "<td>".$data[$a]['dini_hari']."</td>";
-    print "<td>".$data[$a]['suhu']."</td>";
-    print "<td>".$data[$a]['Kelembapan']."</td>";
-    print "</tr>";
-   }
-  ?>
-  </tbody>
+          <thead class="thead-dark">
+            <tr>
+           <th>No</th>
+           <th>Kota</th>
+           <th>Siang</th>
+           <th>Malam</th>
+           <th>Dini hari</th>
+           <th>Suhu</th>
+           <th>Kelembapan</th> 
+            </tr>
+          </thead>
+          <tbody>
+          <?php   
+           for($a=1; $a < count($data); $a++)
+           {
+            print "<tr>";
+            // penomeran otomatis
+            print "<td>".$a."</td>";
+            // menayangkan 
+            print "<td>".$data[$a]['Kota']."</td>";
+            print "<td>".$data[$a]['siang']."</td>";
+            print "<td>".$data[$a]['malam']."</td>";
+            print "<td>".$data[$a]['dini_hari']."</td>";
+            print "<td>".$data[$a]['suhu']."</td>";
+            print "<td>".$data[$a]['Kelembapan']."</td>";
+            print "</tr>";
+           }
+          ?>
+          </tbody>
 </table>
     </div>
 </div>
